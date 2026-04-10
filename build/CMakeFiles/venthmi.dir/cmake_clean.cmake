6eaace9ac60b0d794366b5faa8df1abdaa5c1af1
senkin <senkin7853@gmail.com>
2026-04-10 11:28:48 +0800

    first update

---
+++ build/CMakeFiles/venthmi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/venthmi.dir/src/main.cpp.o"
+  "venthmi.pdb"
+  "venthmi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/venthmi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
